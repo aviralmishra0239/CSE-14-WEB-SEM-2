1f19073aa769168347eb088b22a7474828f3f6ac
Aviral Mishra cse 16_0239 <aviralmishra448@gmail.com>
2026-03-11 00:29:22 +0530

    initial commit

---
+++ rotatednumberpyramid.js
@@ -0,0 +1,20 @@
+function rotatedNumberPyramid(n){
+
+let num=1;
+
+for(let i=1;i<=n;i++){
+
+let row=" ".repeat(n-i);
+
+for(let j=1;j<=i;j++){
+row+=num+" ";
+num++;
+}
+
+console.log(row);
+
+}
+
+}
+
+rotatedNumberPyramid(4);
