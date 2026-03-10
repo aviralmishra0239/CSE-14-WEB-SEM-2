5ae51e7d1781a3325cab03dc422f181b4839e26f
Aviral Mishra cse 16_0239 <aviralmishra448@gmail.com>
2026-03-11 00:17:13 +0530

    initial commit

---
+++ flippedinverted.js
@@ -0,0 +1,17 @@
+function flippedInvertedPyramid(n){
+for(let i=n;i>=1;i--){
+    let row="";
+    
+    for(let j=0;j<n-i;j++){
+        row+=" ";
+    }
+    
+    for(let k=1;k<=i;k++){
+        row+="*";
+    }
+    
+    console.log(row);
+}
+}
+
+flippedInvertedPyramid(5);
